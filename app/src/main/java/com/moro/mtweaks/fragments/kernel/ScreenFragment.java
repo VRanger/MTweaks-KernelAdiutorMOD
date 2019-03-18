SmartPack/SmartPack-Kernel-Manager

Screen: K-Lapse: extend control over time and brightness based modes
Signed-off-by: sunilpaulmathew <sunil.kde@gmail.com>

app/src/main/java/com/grarak/kerneladiutor/fragments/kernel/ScreenFragment.java
@@ -43,6 +43,7 @@ import com.grarak.kerneladiutor.views.recyclerview.CardView;
 import com.grarak.kerneladiutor.views.recyclerview.DropDownView;
 import com.grarak.kerneladiutor.views.recyclerview.GenericSelectView;
 import com.grarak.kerneladiutor.views.recyclerview.RecyclerViewItem;
+import com.grarak.kerneladiutor.views.recyclerview.SelectView;
 import com.grarak.kerneladiutor.views.recyclerview.SeekBarView;
 import com.grarak.kerneladiutor.views.recyclerview.SwitchView;
 
@@ -141,15 +142,16 @@ public class ScreenFragment extends RecyclerViewFragment {
 	klapseCard.setTitle(getString(R.string.klapse));
 
         if (KLapse.hasEnable()) {
-	    SwitchView enable = new SwitchView();
-	    enable.setSummary(getString(R.string.klapse_summary));
-	    enable.setChecked(KLapse.isEnabled());
-	    enable.addOnSwitchListener(new SwitchView.OnSwitchListener() {
-	    	@Override
-	    	public void onChanged(SwitchView switchView, boolean isChecked) {
-		    KLapse.enable(isChecked, getActivity());
-	    	}
-	    });
+            SelectView enable = new SelectView();
+            enable.setSummary(getString(R.string.klapse_summary));
+            enable.setItems(KLapse.enable(getActivity()));
+            enable.setItem(KLapse.getklapseEnable());
+            enable.setOnItemSelected(new SelectView.OnItemSelected() {
+                @Override
+                public void onItemSelected(SelectView selectView, int position, String item) {
+		    KLapse.setklapseEnable(position, getActivity());
+                }
+            });
 
 	    klapseCard.addItem(enable);
 	}

app/src/main/java/com/smartpack/kernelmanager/utils/KLapse.java
@@ -24,6 +24,7 @@ package com.smartpack.kernelmanager.utils;
 import android.content.Context;
 
 import com.grarak.kerneladiutor.fragments.ApplyOnBootFragment;
+import com.grarak.kerneladiutor.R;
 import com.grarak.kerneladiutor.utils.Utils;
 import com.grarak.kerneladiutor.utils.root.Control;
 
@@ -55,16 +56,24 @@ public class KLapse {
     private static final String PULSE_FREQ = KLAPSE + "/pulse_freq";
     private static final String BACKLIGHT_RANGE = KLAPSE + "/backlight_range";
 
-    public static void enable(boolean enable, Context context) {
-        run(Control.write(enable ? "1" : "0", KLAPSE_ENABLE), KLAPSE_ENABLE, context);
-    }
-
-    public static boolean isEnabled() {
-        return Utils.readFile(KLAPSE_ENABLE).startsWith("1");
-    }
-
     public static boolean hasEnable() {
         return Utils.existFile(KLAPSE_ENABLE);
+    }
+
+    public static List<String> enable(Context context) {
+        List<String> list = new ArrayList<>();
+        list.add(context.getString(R.string.turn_off));
+        list.add(context.getString(R.string.time_scale));
+        list.add(context.getString(R.string.bright_scale));
+        return list;
+    }
+
+    public static int getklapseEnable() {
+        return Utils.strToInt(Utils.readFile(KLAPSE_ENABLE));
+    }
+    
+    public static void setklapseEnable(int value, Context context) {
+        run(Control.write(String.valueOf(value), KLAPSE_ENABLE), KLAPSE_ENABLE, context);
     }
 
     public static void setklapseStart(int value, Context context) {

VRanger/MTweaks-KernelAdiutorMOD

Conflicts:
	app/src/main/java/com/moro/mtweaks/fragments/kernel/ScreenFragment.java
	app/src/main/java/com/moro/mtweaks/utils/KLapse.java

app/src/main/java/com/moro/mtweaks/fragments/kernel/ScreenFragment.java
@@ -42,6 +42,7 @@ import com.moro.mtweaks.views.recyclerview.CardView;
 import com.moro.mtweaks.views.recyclerview.DropDownView;
 import com.moro.mtweaks.views.recyclerview.GenericSelectView;
 import com.moro.mtweaks.views.recyclerview.RecyclerViewItem;
+import com.moro.mtweaks.views.recyclerview.SelectView;
 import com.moro.mtweaks.views.recyclerview.SeekBarView;
 import com.moro.mtweaks.views.recyclerview.SwitchView;
 
@@ -136,15 +137,16 @@ public class ScreenFragment extends RecyclerViewFragment {
 	klapseCard.setTitle(getString(R.string.klapse));
 
         if (KLapse.hasEnable()) {
-	    SwitchView enable = new SwitchView();
-	    enable.setSummary(getString(R.string.klapse_summary));
-	    enable.setChecked(KLapse.isEnabled());
-	    enable.addOnSwitchListener(new SwitchView.OnSwitchListener() {
-	    	@Override
-	    	public void onChanged(SwitchView switchView, boolean isChecked) {
-		    KLapse.enable(isChecked, getActivity());
-	    	}
-	    });
+            SelectView enable = new SelectView();
+            enable.setSummary(getString(R.string.klapse_summary));
+            enable.setItems(KLapse.enable(getActivity()));
+            enable.setItem(KLapse.getklapseEnable());
+            enable.setOnItemSelected(new SelectView.OnItemSelected() {
+                @Override
+                public void onItemSelected(SelectView selectView, int position, String item) {
+		    KLapse.setklapseEnable(position, getActivity());
+                }
+            });
 
 	    klapseCard.addItem(enable);
 	}

app/src/main/java/com/moro/mtweaks/utils/KLapse.java
@@ -24,6 +24,7 @@ package com.moro.mtweaks.utils;
 import android.content.Context;
 
 import com.moro.mtweaks.fragments.ApplyOnBootFragment;
+import com.moro.mtweaks.R;
 import com.moro.mtweaks.utils.Utils;
 import com.moro.mtweaks.utils.root.Control;
 
@@ -55,16 +56,24 @@ public class KLapse {
     private static final String PULSE_FREQ = KLAPSE + "/pulse_freq";
     private static final String BACKLIGHT_RANGE = KLAPSE + "/backlight_range";
 
-    public static void enable(boolean enable, Context context) {
-        run(Control.write(enable ? "1" : "0", KLAPSE_ENABLE), KLAPSE_ENABLE, context);
-    }
-
-    public static boolean isEnabled() {
-        return Utils.readFile(KLAPSE_ENABLE).startsWith("1");
-    }
-
     public static boolean hasEnable() {
         return Utils.existFile(KLAPSE_ENABLE);
+    }
+
+    public static List<String> enable(Context context) {
+        List<String> list = new ArrayList<>();
+        list.add(context.getString(R.string.turn_off));
+        list.add(context.getString(R.string.time_scale));
+        list.add(context.getString(R.string.bright_scale));
+        return list;
+    }
+
+    public static int getklapseEnable() {
+        return Utils.strToInt(Utils.readFile(KLAPSE_ENABLE));
+    }
+    
+    public static void setklapseEnable(int value, Context context) {
+        run(Control.write(String.valueOf(value), KLAPSE_ENABLE), KLAPSE_ENABLE, context);
     }
 
     public static void setklapseStart(int value, Context context) {